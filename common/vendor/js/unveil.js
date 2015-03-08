2249a19dd3d6f1c976b19ee7d11c7a1cbf830a53
dannycochran <daniel@cochrans.org>
2015-03-08 14:48:57 -0700

    unveil fix

--- common/vendor/js/unveil.js
+++ common/vendor/js/unveil.js
@@ -22,6 +22,7 @@
       var source = this.getAttribute(attrib);
       source = source || this.getAttribute('data-src');
       if (source) {
+        this.removeAttribute('data-src');
         this.setAttribute('src', source);
         if (typeof callback === 'function') callback.call(this);
       }
